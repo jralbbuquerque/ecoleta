7361d80eac53181fdc64cacb186a015ba96c124d
Francisco Júnior <jralbbuquerque@gmail.com>
2020-06-16 11:27:57 -0300

    Adiciona mapa na página de criar ponto de coleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'; 
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -59,6 +60,15 @@ const CreatePoint = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[ -1.295456, -47.888707 ]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[ -1.296079, -47.8913228 ]}/>
+          </Map>
 
           <div className="field-group">
             <div className="field">
